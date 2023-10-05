4023070af3d49b649130fd220af0cef8a2d05076
BENDINELLI Tommaso <tbe@l7286.csem.local>
2023-10-05 11:28:38 +0200

    added hydra automatic commit

--- src/experiment_tracker/hydra_utils.py
+++ src/experiment_tracker/hydra_utils.py
@@ -18,3 +18,28 @@ def save_hash(cfg):
         with open("commit_info.yaml", "w") as f:
             json.dump(save_dict, f, indent=4)
 
+
+def log_book():
+    import logging 
+    import os 
+    
+    # Create the asset path and the logbook if it does not exist
+    log_book_path = Path(hydra.utils.to_absolute_path("assets/logbook.json"))
+
+    log_book_path.parent.mkdir(parents=True, exist_ok=True)
+    Path(log_book_path).touch()
+
+    # Save to the logbook that the file has been run 
+    
+    # Create a logger
+    logger = logging.getLogger(__file__)
+    # Set the log level
+    logger.setLevel(logging.INFO)
+    # Create a file handler
+    file_handler = logging.FileHandler(log_book_path)
+    # Set the log format
+    file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    # Add the file handler to the logger
+    logger.addHandler(file_handler)
+    # Add message to the logbook
+    return logger
