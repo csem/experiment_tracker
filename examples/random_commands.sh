9b9ff98a865b4cce3161cba7021b2dbc48f3f1f2
BENDINELLI Tommaso <tbe@l7286.csem.local>
2024-02-01 08:22:11 +0100

    Adding more command

--- examples/random_commands.sh
+++ examples/random_commands.sh
@@ -1,3 +1,5 @@
 python3 examples/training_experiment.py -m seed=1,2,3,43 dataset=digits,iris normalize=True,False
-python3 examples/training_experiment.py -m estimator=svm,knn dataset=digits,iris normalize=True,False
-python3 examples/training_experiment.py -m trigger_error=True dataset=digits
+python3 examples/training_experiment.py -m estimator=svm dataset=digits
+python3 examples/training_experiment.py -m estimator=svm dataset=digits normalize=False
+python3 examples/training_experiment.py -m estimator=svm,knn dataset=iris normalize=True,False
+python3 examples/training_experiment.py trigger_error=True dataset=digits
